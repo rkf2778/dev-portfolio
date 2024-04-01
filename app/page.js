582ef43482e5b13e5844062fb8ef89d79331319d
Rohit Kochikkat Francis <rohit.kf.dev@gmail.com>
2024-04-01 19:32:11 +0100

    Open to work Component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import OpenToWork from "@/components/OpenToWork";
 
 export default async function Home() {
   return (
     <>
+      <OpenToWork/>
       <About />
       <Skills />
       <Projects/>
---
+++ components/OpenToWork.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function OpenToWork() {
+  return (
+    <div className="fixed left-10 top-10 flex place-items-center gap-3 border-4 border-white px-4 py-2">
+      <div class="shadow-outline h-3 w-3 rounded-full bg-green-500 dot"></div>
+      <div className="text-lg">Open to Work</div>
+    </div>
+  )
+}
+
+export default OpenToWork
