bbd2304a9f639ba42addf9203c62db5d934f5fa7
Rohit Kochikkat Francis <rohit.kf.dev@gmail.com>
2024-04-03 11:59:10 +0100

    Fix alignment

--- components/OpenToWork.jsx
+++ components/OpenToWork.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function OpenToWork() {
   return (
-    <div className="fixed left-1 md:left-10 top-1 md:top-10 z-10 flex place-items-center gap-3 border-2 md:border-4 border-white bg-gray-800 px-4 py-2">
+    <div className="fixed left-2 md:left-10 top-2 md:top-10 z-10 flex items-center gap-1 md:gap-3 border-2 md:border-4 border-white bg-gray-800 px-1 md:px-4 py-2 md:py-2">
       <div class="shadow-outline dot h-3 w-3 rounded-full bg-green-500"></div>
       <div className="text-xs md:text-sm">Open to Work</div>
     </div>
