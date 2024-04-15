9c0533c2e3ae35b26f8e7109f51a012b1cc34e04
Rohit Kochikkat Francis <rohit.kf.dev@gmail.com>
2024-04-15 11:38:07 +0100

    Removed unwanted link

--- app/layout.js
+++ app/layout.js
@@ -13,16 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
-      </head>
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <HydrationOverlay>
           {children}
