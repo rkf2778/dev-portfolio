90dd6f2d1191f31c68f76c9afc0fbab4c44a0ff6
Rohit Kochikkat Francis <rohit.kf.dev@gmail.com>
2024-05-30 10:40:06 +0100

    Update Styles

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -9,19 +9,19 @@ const expList = [
   },
   {
     title: "Wipro",
-    as: "Project Engineer",
+    as: "Project Engineer (2019 - 2021)",
   },
   {
     title: "Chargebee",
-    as: "Frontend Engineer",
+    as: "Frontend Engineer (2021 - 2022)",
   },
   {
     title: "University of Greenwich",
-    as: "MSc Data Science",
+    as: "MSc Data Science (2023 - 2024)",
   },
   {
-    title: "NEXT ???",
-    as: "",
+    title: "???",
+    as: "Data Scientist",
   },
 ]
 
@@ -36,15 +36,18 @@ function Experience() {
             className="relative w-full max-w-[470px] rounded-xl border px-6 py-5 text-center before:absolute before:inset-0 before:rounded-[11px] before:border-t before:border-white before:bg-gradient-to-b before:from-gray-100 before:shadow dark:border-white/15 dark:bg-gray-900 dark:before:border-white/20 dark:before:from-white/10 dark:before:to-transparent  dark:before:shadow-gray-950"
           >
             <p
-              className={cn("mb-2 font-bold uppercase text-yellow-600", {
-                "text-white": exp.title == "? ? ?",
-              })}
+              className={cn(
+                "mb-2 bg-gradient-to-tr from-[#FF8660] to-[#8000FF] bg-clip-text font-extrabold uppercase text-transparent",
+                {
+                  "text-white": exp.title == "???",
+                }
+              )}
             >
               {exp.title}
             </p>
             <p
               className={cn("text-slate-50-900  font-bold", {
-                "bg-gradient-to-tr from-[#FF8660] to-[#8000FF] bg-clip-text uppercase text-transparent text-2xl":
+                "bg-gradient-to-tr from-[#FF8660] to-[#ff2d2d] bg-clip-text text-2xl uppercase text-transparent":
                   exp.as == "Data Scientist",
               })}
             >
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,7 +14,7 @@ function Skills() {
             </h2>
             <p className="mt-1 text-gray-700 dark:text-gray-300">
               <span className="bg-gradient-to-tr from-[#FF8660] to-[#8000FF] bg-clip-text text-transparent">
-                Jack of all trades, master of none 😉
+              😉 Jack of all trades, master of none 😉
               </span>
             </p>
           </div>
